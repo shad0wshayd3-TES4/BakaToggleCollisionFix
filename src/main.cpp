3ab8ac54d3b22db641dc5a6b5e5f5a837366eb7c
shad0wshayd3 <2724172+shad0wshayd3@users.noreply.github.com>
2025-06-12 10:06:21 -0600

    feat: Address Library support

--- src/main.cpp
+++ src/main.cpp
@@ -7,24 +7,31 @@ namespace Hooks
 		static bool ToggleCollision(const RE::SCRIPT_PARAMETER*, const char*, RE::TESObjectREFR*, RE::TESObjectREFR*, RE::Script*, RE::ScriptLocals*, double&, std::uint32_t&)
 		{
 			UE::UCheatManager* CheatManager{ nullptr };
-			if (auto Engine = UE::UEngine::GetSingleton()) {
-				if (auto World = Engine->GetCurrentPlayWorld(nullptr)) {
-					if (auto PlayerController = UE::UGameplayStatics::GetPlayerController(World, 0)) {
+			if (auto Engine = UE::UEngine::GetSingleton())
+			{
+				if (auto World = Engine->GetCurrentPlayWorld(nullptr))
+				{
+					if (auto PlayerController = UE::UGameplayStatics::GetPlayerController(World, 0))
+					{
 						CheatManager = *REX::ADJUST_POINTER<UE::UCheatManager*>(PlayerController, 0x400);
 					}
 				}
 			}
 
-			if (CheatManager) {
-				if (bCollisionEnabled) {
+			if (CheatManager)
+			{
+				if (bCollisionEnabled)
+				{
 					CheatManager->Ghost();
 				}
-				else {
+				else
+				{
 					CheatManager->Walk();
 				}
 
 				bCollisionEnabled = !bCollisionEnabled;
-				if (auto MenuConsole = RE::MenuConsole::Instance(true)) {
+				if (auto MenuConsole = RE::MenuConsole::Instance(true))
+				{
 					MenuConsole->PrintLine("Collision -> %s", bCollisionEnabled ? "On" : "Off");
 				}
 			}
@@ -33,7 +40,8 @@ namespace Hooks
 
 		static void Install()
 		{
-			if (auto func = RE::SCRIPT_FUNCTION::LocateConsoleCommand("ToggleCollision"sv)) {
+			if (auto func = RE::SCRIPT_FUNCTION::LocateConsoleCommand("ToggleCollision"sv))
+			{
 				func->executeFunction = ToggleCollision;
 			}
 		}
@@ -41,27 +49,25 @@ namespace Hooks
 
 	class hkDisableText
 	{
-	public:
+	private:
 		static void ClientMessage(void*, void*, UE::FName, float)
 		{
-			// may do something with this later
-			// _ClientMessage(a_this, a_msg, a_type, a_duration);
 			return;
 		}
 
-		inline static REL::Hook _ClientMessage{ REL::Offset(0x2F7FD20), 0x64, ClientMessage };
+		inline static REL::Hook _ClientMessage{ REL::ID(211827), 0x64, ClientMessage };
 	};
 
 	class hkLoadGame
 	{
-	public:
+	private:
 		static void LoadGame(void* a_this)
 		{
 			hkToggleCollision::bCollisionEnabled = true;
 			return _LoadGame(a_this);
 		}
 
-		inline static REL::Hook _LoadGame{ REL::Offset(0x670CDC0), 0x1D12, LoadGame };
+		inline static REL::Hook _LoadGame{ REL::ID(409639), 0x1D12, LoadGame };
 	};
 
 	static void Install()
